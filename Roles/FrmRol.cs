592bc6bfa4db4afe3453995a85c1befd08d9982e
Armando Meza <amezal01@hotmail.com>
2022-05-29 17:05:29 -0600

    interfaz roles

---
+++ Roles/FrmRol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ScapProject0.Roles
+{
+    public partial class FrmRol : Gtk.Window
+    {
+        public FrmRol() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
