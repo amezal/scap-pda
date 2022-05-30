464b0969f2fd503345b2fb53b45e0c087fd3690a
Armando Meza <amezal01@hotmail.com>
2022-05-30 03:25:44 -0600

    negocios rolopcion

---
+++ Negocios/Ng_tbl_rolOpcion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using ScapProject0.Datos;
+namespace ScapProject0.Negocios
+{
+    public class Ng_tbl_rolOpcion
+    {
+        Conexion con = new Conexion();
+        StringBuilder sb = new StringBuilder();
+
+        public bool existe(int idRol, int idOpcion)
+        {
+            bool ex = false; //bandera
+            IDataReader idr = null;
+            sb.Clear();
+            sb.Append($"SELECT * FROM LMBA.rolOpcion WHERE id_rol={idRol} AND id_opcion={idOpcion}");
+            //Console.WriteLine(sb.ToString());
+            try
+            {
+                con.AbrirConexion();
+                idr = con.Leer(CommandType.Text, sb.ToString());
+                if (idr.Read())
+                {
+                    ex = true;
+                }
+                return ex;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+            finally
+            {
+                idr.Close();
+                con.CerrarConexion();
+            }
+        }//fin del metodo
+    }
+}
