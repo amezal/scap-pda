3780f974453643799ab59c0d006e5fa2272136cb
Armando Meza <amezal01@hotmail.com>
2022-05-30 01:03:23 -0600

    llenar datos mod rol

--- Roles/FrmModRol.cs
+++ Roles/FrmModRol.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using ScapProject0.Entidades;
+using ScapProject0.Datos;
+using ScapProject0.Negocios;
+using Gtk;
+
 namespace ScapProject0.Roles
 {
     public partial class FrmModRol : Gtk.Window
     {
         FrmRol caller;
         private int idRol;
+        private Tbl_rol rol;
+        private Dt_tbl_rol dtrol = new Dt_tbl_rol();
+        private Dt_tbl_Opcion dtop = new Dt_tbl_Opcion();
+        private Dt_tbl_rolOpcion dtrolop = new Dt_tbl_rolOpcion();
 
         public FrmRol Caller { get => caller; set => caller = value; }
         public int IdRol { get => idRol; set => idRol = value; }
@@ -14,6 +23,8 @@ namespace ScapProject0.Roles
         {
             this.Build();
             idRol = id;
+            rol = dtrol.DatosRol(idRol);
+            this.LlenarCampos();
         }
 
         protected void OnBtnRegresarClicked(object sender, EventArgs e)
@@ -26,6 +37,35 @@ namespace ScapProject0.Roles
         {
             this.caller.Show();
             this.Hide();
+        }
+
+        protected void LlenarCampos()
+        {
+            this.txtID.Text = rol.Id_rol.ToString();
+            this.txtRol.Text = rol.Rol;
+
+            TreeModel model = dtop.listarOpcion();
+            model.GetIterFirst(out TreeIter ti);
+            do
+            {
+                int id = Convert.ToInt32(model.GetValue(ti, 0));
+                string nombre = model.GetValue(ti, 1).ToString();
+                model.SetValue(ti, 0, nombre);
+                model.SetValue(ti, 1, id.ToString());
+            } while (model.IterNext(ref ti));
+
+            cbxOpcion.Model = model;
+
+            this.trvwRolOpcion.Model = dtrolop.ListarRolOpcion(idRol);
+            string[] titulos = { "Opcion", "", "", ""};
+
+            for (int i = 0; i < titulos.Length; i++)
+            {
+                this.trvwRolOpcion.AppendColumn(titulos[i], new CellRendererText(), "text", i);
+            }
+            this.trvwRolOpcion.Columns[1].Visible = false;
+            this.trvwRolOpcion.Columns[2].Visible = false;
+            this.trvwRolOpcion.Columns[3].Visible = false;
         }
     }
 }
