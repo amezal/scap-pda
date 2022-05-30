205d3876c2d6874f5580a8c2015b6cc18ce3b590
Armando Meza <amezal01@hotmail.com>
2022-05-30 00:06:43 -0600

    mod rol interfaz

---
+++ Roles/FrmModRol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ScapProject0.Roles
+{
+    public partial class FrmModRol : Gtk.Window
+    {
+        public FrmModRol() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
